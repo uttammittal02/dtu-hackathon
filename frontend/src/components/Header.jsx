1777606abbf863214e6596f517fae290d1f85c91
Harsh Johar <hpsj3112@gmail.com>
2022-05-28 15:47:17 +0530

    Header basic

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import './css/Header.css';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <div className="Header__logo">
+                <Link to="/">
+
+                <img src="logo192.png" alt="" />
+                </Link>
+            </div>
+            <div className="Header__options">
+                <NavbarOption name={"Home"} link={"/"} />
+                <NavbarOption name={"About"} link={"/about"} />
+                <NavbarOption name={"Results"} link={"/results"} />
+                <NavbarOption name={"Predict"} link={"/predict"} />
+            </div>
+
+            <div className="Header__buttons">
+                <button>Login</button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
+
+const NavbarOption = ({ name, link }) => {
+    return (
+        <div className="Header__option">
+            <Link to={link} className="Header__link">{name}</Link>
+        </div>
+    );
+};
